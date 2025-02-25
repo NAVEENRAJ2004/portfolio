92c7a7ebac616edf5be15d57e4079430974edaac
NAVEENRAJ2004 <naveenraj10203040@gmail.com>
2025-02-25 22:11:26 +0530

    updated portfolio

---
+++ config.js
@@ -0,0 +1,53 @@
+window.PorfolioConfig = {
+  name: 'Naveenraj',
+  jobTitle: 'SOFTWARE DEVELOPER',
+  jobDescription: 'Vice President @UAV-DROBOTICS | Software Developer | Flutter | Docker | Drones & Robotics | Graphic Designer | Pre-final Year Student at Sathyabama University',
+  skills: [
+    {
+      name: 'WEB DEVELOPMENT',
+      description: 'Experienced in building responsive and scalable web applications using modern frameworks like MERN stack, ensuring seamless user experiences.'
+    },
+    {
+      name: 'FLUTTER',
+      description: 'Proficient in developing cross-platform mobile applications with Flutter and Dart, integrating APIs for dynamic and real-time functionalities.'
+    },
+    {
+      name: 'DRONES & ROBOTICS',
+      description: 'Hands-on experience in designing and programming autonomous drones, integrating AI capabilities, and optimizing robotic systems for various applications.'
+    },
+    {
+      name: 'GRAPHIC DESIGN',
+      description: 'Skilled in Photoshop, Figma, and Blender, crafting visually appealing designs for branding, UI/UX, and multimedia content creation.'
+    }
+  ],
+  works: [
+    {
+      name: 'TEDXSIST WEBSITE',
+      link: 'https://tedxsist.com',
+      description: 'The TEDxSIST website was built using React.js and Next.js for a dynamic and responsive frontend, styled with Tailwind CSS for a modern UI. The backend was developed with Node.js and Express.js, utilizing MongoDB with Mongoose ORM for efficient database management. Authentication was implemented using JWT-based security, while RESTful APIs handled dynamic content updates. The site was deployed on Vercel for the frontend and AWS for backend services, with server-side rendering (SSR) and lazy loading optimizing performance. Security features included HTTPS, rate limiting, and input validation, ensuring a secure and seamless user experience.'
+    },
+    {
+      name: 'STONKS (STOCK QUOTATION MOBILE APPLICATION)',
+      link: 'https://github.com/NAVEENRAJ2004/STOCK-QUOTE-APP',
+      description: 'Esse magna anim incididunt enim sint aute consectetur ipsum id nostrud eu do. Pariatur et anim enim aliquip anim. Laboris labore incididunt labore enim nisi elit excepteur deserunt irure exercitation exercitation. Lorem sit ad laborum sit velit veniam sit nisi nostrud occaecat duis nulla duis.'
+    }
+  ],
+  contacts: {
+    email: 'naveenrajultd@gmail.com',
+    phoneNo: '+91 7708503621',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/NAVEENRAJ2004'
+      },
+      {
+        name: 'Linkedin',
+        link: 'https://www.linkedin.com/in/naveenrajk30'
+      },
+      {
+        name: 'Instagram',
+        link: 'https://www.instagram.com/am_naveenraj/'
+      }
+    ]
+  }
+};
